501f6336f2ebe9d947acbbab2d8901c760b83fdd
Patrick Pun <panyang1217@hotmail.com>
2017-03-09 17:09:47 +0800

    Add Angular quickstarter

---
+++ src/vendor.ts
@@ -0,0 +1,9 @@
+import "@angular/common";
+import "@angular/compiler";
+import "@angular/core";
+import "@angular/http";
+import "@angular/router";
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+
+import 'rxjs/Rx';
