b1845547216d5c808714623f706c4fbe8d1ff6db
Patrick Pun <panyang1217@hotmail.com>
2017-04-18 22:54:10 +0800

    Fix path configuration

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,16 @@
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var htmlPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 var common = require('./webpack.common.js');
+
+var outPath = path.resolve('./build/dev');
 
 // referenced: https://webpack.js.org/guides/production-build/
 module.exports = webpackMerge(common, {
     output: {
-        path: './build/dev'
+        path: outPath
     },
     plugins: [
         new webpack.DefinePlugin({
